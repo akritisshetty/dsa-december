c64d2edde392dcf946178087e3d0e79d4592a598
Akriti S Shetty <akritishetty.in@gmail.com>
2025-12-21 15:19:46 +0530

    Maximum Number of Words Found in Sentences function

---
+++ MaximumNumberOfWordsFoundInSentences.py
@@ -0,0 +1,5 @@
+# Maximum Number of Words Found in Sentences
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max(sentence.count(" ") + 1 for sentence in sentences)
