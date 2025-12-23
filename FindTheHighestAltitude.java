42e47b023e315e4e7bf0eaa52f14c6432b55c6ed
Akriti S Shetty <akritishetty.in@gmail.com>
2025-12-24 00:13:22 +0530

    Find the Highest Altitude function in java

---
+++ FindTheHighestAltitude.java
@@ -0,0 +1,14 @@
+// Find the Highest Altitude
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            currentAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+
+        return maxAltitude;
+    }
+}
