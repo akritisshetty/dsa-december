accb9049ee8f67eec7b6f45d69a14f30012ba945
Akriti S Shetty <akritishetty.in@gmail.com>
2025-12-25 07:19:42 +0530

    Product of Array Except Self function in C

---
+++ ProductOfArrayExceptSelf.c
@@ -0,0 +1,19 @@
+// Product of Array Except Self
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int* answer = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+
+    int prefix = 1;
+    for (int i = 0; i < numsSize; i++) {
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    int suffix = 1;
+    for (int i = numsSize - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return answer;
+}
