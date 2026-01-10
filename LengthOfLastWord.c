5591a122cdd17498ed4012ccd7514c2cd516d0af
Akriti S Shetty <akritishetty.in@gmail.com>
2026-01-10 06:30:08 +0530

    Length of Last Word function in C

---
+++ LengthOfLastWord.c
@@ -0,0 +1,17 @@
+// Length of Last Word
+
+int lengthOfLastWord(char* s) {
+    int len = strlen(s);
+    int count = 0;
+
+    while (len > 0 && s[len - 1] == ' ') {
+        len--;
+    }
+
+    while (len > 0 && s[len - 1] != ' ') {
+        count++;
+        len--;
+    }
+
+    return count;
+}
